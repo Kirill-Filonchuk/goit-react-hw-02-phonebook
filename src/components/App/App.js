cb2e6b6311fb2d6aa05b83fda98e6c51f73e9aa6
Kirill Filonchuk <kir.realtor@gmail.com>
2021-08-31 00:18:11 +0300

    task 1 and 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,62 +1,103 @@
 import '../App/App.css';
 import React, { Component } from 'react';
 import shortid from 'shortid';
-import Section from '../Section';
-import FeedbackOptions from '../FeedbackOptions';
-import Statistics from '../Statistics';
+
+// import Section from '../Section';
+// import FeedbackOptions from '../FeedbackOptions';
+// import Statistics from '../Statistics';
 
 class App extends Component {
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+    contact: [],
+    name: '',
+    number: '',
   };
 
-  handleDecrement = e => {
-    console.dir(e.target.name);
-    const target = e.target.name;
-    if (typeof target == 'undefined') {
-      console.dir(target);
-      return;
-    }
-    this.setState(() => ({
-      [e.target.name]: this.state[e.target.name] + 1,
-    }));
+  formSubmitHandler = ({ name, number }) => {
+    const cont = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+    console.log(cont);
+
+    this.setState(prevState => {
+      const contact = [...prevState.contact, cont];
+      return {
+        contact,
+        name: '',
+        number: '',
+      };
+    });
+    console.log('Arr', this.state.contact);
   };
 
-  totalCount = () => {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    // console.log(this.state.good);
-    return total;
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    // используем вычисляемые св-ва
+    this.setState({
+      [name]: value,
+    });
   };
 
-  positivePercentage = () => {
-    // console.log(this.totalCount());
-    if (this.totalCount() > 0) {
-      const positivPerc = Math.floor((this.state.good * 100) / this.totalCount());
-      // console.log(positivPerc);
-      return positivPerc;
-    }
-    // or to the string - so, we can do whith out isNaN
-    // const positivPerc = `${Math.floor((this.state.good * 100) / this.totalCount())}`;
-    // console.log(positivPerc.length);
-    return 0;
+  handleSubmit = e => {
+    e.preventDefault();
+    const { name, number } = this.state;
+    console.log(`
+      name: ${name}
+      number: ${number}
+    `);
+    this.formSubmitHandler({ ...this.state });
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
   };
 
   render() {
     return (
-      <div key={shortid.generate()} className="container">
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions options={this.state} onLeaveFeedback={this.handleDecrement} />
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.totalCount()}
-            positivePercentage={this.positivePercentage()}
+      <div className="container">
+        <h1>Phonebook</h1>
+        <br />
+        <form onSubmit={this.handleSubmit}>
+          Name
+          <br />
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+            value={this.state.name}
+            onChange={this.handleChange}
           />
-        </Section>
+          <br />
+          Number
+          <br />
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            required
+            value={this.state.number}
+            onChange={this.handleChange}
+          />
+          <br />
+          <button type="submit">Add contact</button>
+        </form>
+        <br />
+        <h2>Contacts</h2>
+        <ul>
+          {this.state.contact.map(({ name, number }, index) => (
+            <div key={index}>
+              <li>
+                {name}:<span>{number}</span>
+              </li>
+            </div>
+          ))}
+        </ul>
       </div>
     );
   }
